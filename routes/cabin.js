a11fdd3c351c2e34bad59d582be78332fca8926d
Layek Miah <layekofficial63@gmail.com>
2025-11-09 10:27:35 +0600

    created cabin api

--- routes/cabin.js
+++ routes/cabin.js
@@ -1,7 +1,10 @@
-const express = require('express');
+const express = require("express");
+const { getCabins, getCabin } = require("../controller/cabin.controller");
 
 const router = express.Router();
 
-
+router.get("/", getCabins);
+router.get("/:id", getCabin);
+router.get("/:id/cabin/price");
 
 module.exports = router;
