978ef58a4f0bcd65022f67a0cc06ccdc245ab648
Layek Miah <layekofficial63@gmail.com>
2025-11-10 01:07:39 +0600

    problem solving

--- controller/guest.controller.js
+++ controller/guest.controller.js
@@ -1,5 +1,4 @@
-const connectDB = require("../config/db");
-const Guest = require("../model/guest");
+const Guest = require('../model/guest');
 
 // exports.createGuest = async (req, res) => {
 //   const { fullName, email } = req.body;
@@ -34,7 +33,6 @@ const Guest = require("../model/guest");
 // };
 
 exports.getGuest = async (req, res) => {
-connectDB()
   const guest = await Guest.find();
   res.status(2001).json(guest);
   // const email = req.params.email;
