8244578998243ab302d31083fa14d208d3b056c9
Layek Miah <layekofficial63@gmail.com>
2025-11-09 23:24:16 +0600

    error

--- index.js
+++ index.js
@@ -23,6 +23,8 @@ app.use(express.json());
 
 connectDB();
 
+const PORT = process.env.PORT || 3000;
+
 const routes = [
   { path: "/api/bookings", router: booking },
   { path: "/api/cabins", router: cabin },
@@ -34,6 +36,10 @@ routes.forEach(({ path, router }) => app.use(path, router));
 
 app.get("/", (req, res) => {
   res.send("The Wild Oasis Website Backend is working");
+});
+
+app.listen(PORT, () => {
+  console.log("server running on port ..");
 });
 
 module.exports = app;
